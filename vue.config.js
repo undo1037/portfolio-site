2627c44594cd3a0ad2c18d1498593e42a597171b
undo <undo1037@gmail.com>
2021-03-07 19:10:53 +0900

    update: build設定の変更

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  publicPath: '/portfolio-site',
-  outputDir: 'docs',
-  filenameHashing: false,
-  productionSourceMap: false,
+  baseUrl: '/portfolio-site',
+  outputDir: 'docs'
 }
